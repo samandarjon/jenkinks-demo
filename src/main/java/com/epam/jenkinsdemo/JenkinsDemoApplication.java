611a104bf05353a8a860652a6409237970d4d3cf
Samandar Akbarov <akbarov.samandar@gmail.com>
2023-03-07 20:59:24 +0500

    change app port and add hello api

--- src/main/java/com/epam/jenkinsdemo/JenkinsDemoApplication.java
+++ src/main/java/com/epam/jenkinsdemo/JenkinsDemoApplication.java
@@ -2,12 +2,20 @@ package com.epam.jenkinsdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class JenkinsDemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(JenkinsDemoApplication.class, args);
     }
+    @GetMapping
+    public String sayHello(){
+        return "Hey, How are you?";
+    }
+
 
 }
